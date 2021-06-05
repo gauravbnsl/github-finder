a08c87b3d4033a33a91e1d1b799fafe5ac839939
Gaurav Bansal <gauravbansal67@hotmail.com>
2021-06-05 19:11:23 +0530

    fix: mobile layout

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
-import UserItem from './UserItem'
+import React, { Component } from "react";
+import UserItem from "./UserItem";
 
 class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map((user) => {
-            return <UserItem key={user.id} user={user}/>;
-        })}
-      </div>
-    );
-  }
-}
-
-const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
+	render() {
+		return (
+			<div className="user-wrapper">
+				{this.props.users.map((user) => {
+					return <UserItem key={user.id} user={user} />;
+				})}
+			</div>
+		);
+	}
 }
 
 export default Users;
