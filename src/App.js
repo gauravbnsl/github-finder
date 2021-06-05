6140edc5d554d703750e8f5611406125ea1c82fa
Gaurav Bansal <gauravbansal67@hotmail.com>
2021-06-05 18:38:53 +0530

    fix: css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+// import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import { data } from "./data";
 
 class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  };
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get('https://api.github.com/users');
-    console.log(res.data);
-    this.setState({ users: res.data, loading: false });
-  }
-  render() {
-    return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
+	state = {
+		users: [],
+		loading: false,
+	};
+	async componentDidMount() {
+		this.setState({ loading: true });
+		// const res = await axios.get('https://api.github.com/users');
+		const res = data;
+		debugger;
+		console.log(res);
+		this.setState({ users: res, loading: false });
+	}
+	render() {
+		return (
+			<div className="App">
+				<Navbar />
+				<div className="container">
+					<Users loading={this.state.loading} users={this.state.users} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Navbar = ({title}) => {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>{title}</h1>
-      </nav>
-    );
-}
+const Navbar = ({ title }) => {
+	return (
+		<nav className="navbar">
+			<h1>{title}</h1>
+		</nav>
+	);
+};
 
 Navbar.defaultProps = {
-  title: 'Github Finder',
+	title: "Github Finder",
 };
 
 Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
 };
-
 
 export default Navbar;
